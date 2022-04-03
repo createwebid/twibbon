51cba6ed198d5d903e1c011e41cdec19ddd02b91
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2022-04-04 00:21:19 +0700

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -4,65 +4,65 @@ var quotes=new Array()
 
 quotes[0]='<h3>There are some people who live in a dream world, and there are some who face reality; and then there are those who turn one into the other.<h3>'
 
-quotes[1]='<h3>Whether you think you can or whether you think you can\'t, you\'re right!.</p> <p><cite>-Henry Ford</cite></p>'
+quotes[1]='<h3>Whether you think you can or whether you think you can\'t, you\'re right!.</h3>'
 
-quotes[2]='<h3>I know of no more encouraging fact than the unquestionable ability of man to elevate his life by conscious endeavor.</p> <span class="p-pull-quote__citation">-Henry David Thoreau</span>'
+quotes[2]='<h3>I know of no more encouraging fact than the unquestionable ability of man to elevate his life by conscious endeavor.</h3>'
 
-quotes[3]='<h3>Do not let what you cannot do interfere with what you can do.</p> <p><cite>-John Wooden</cite></p>'
+quotes[3]='<h3>Do not let what you cannot do interfere with what you can do.</h3>'
 
-quotes[4]='<h3>Accept everything about yourself - I mean everything, You are you and that is the beginning and the end - no apologies, no regrets.<p> <span class="p-pull-quote__citation">-Clark Moustakas</span>'
+quotes[4]='<h3>Accept everything about yourself - I mean everything, You are you and that is the beginning and the end - no apologies, no regrets.</h3>'
 
-quotes[5]='<h3>We must accept life for what it actually is - a challenge to our quality without which we should never know of what stuff we are made, or grow to our full stature.<p> <span class="p-pull-quote__citation">-Ida R. Wylie</span>'
+quotes[5]='<h3>We must accept life for what it actually is - a challenge to our quality without which we should never know of what stuff we are made, or grow to our full stature.</h3>'
 
-quotes[6]='<h3>High achievement always takes place in the framework of high expectation.<p> <span class="p-pull-quote__citation">-Jack Kinder</span>'
+quotes[6]='<h3>High achievement always takes place in the framework of high expectation.</h3>'
 
-quotes[7]='<h3>The measure of a man is the way he bears up under misfortune.<p> <span class="p-pull-quote__citation">-Plutarch</span>'
+quotes[7]='<h3>The measure of a man is the way he bears up under misfortune.</h3>'
 
-quotes[8]='<h3>Don\'t wait for your ship to come in, swim out to it.<p> <span class="p-pull-quote__citation">-Anon</span>'
+quotes[8]='<h3>Don\'t wait for your ship to come in, swim out to it.</h3>'
 
-quotes[9]='<h3>As I grow older, I pay less attention to what men say. I just watch what they do.<p> <span class="p-pull-quote__citation">-Andrew Carnegie</span>'
+quotes[9]='<h3>As I grow older, I pay less attention to what men say. I just watch what they do.</h3>'
 
-quotes[10]='<h3>No steam or gas ever drives anything until it is confined. No Niagara is ever turned into light and power until it is tunneled. No life ever grows until it is focused, dedicated, disciplined.<p> <span class="p-pull-quote__citation">-Harry Emerson Fosdick</span>'
+quotes[10]='<h3>No steam or gas ever drives anything until it is confined. No Niagara is ever turned into light and power until it is tunneled. No life ever grows until it is focused, dedicated, disciplined.</h3>'
 
-quotes[11]='<h3>The words printed here are concepts. You must go through the experiences.<p> <span class="p-pull-quote__citation">-Carl Frederick</span>'
+quotes[11]='<h3>The words printed here are concepts. You must go through the experiences.</h3>'
 
-quotes[12]='<h3>Man cannot discover new oceans unless he has the courage to lose sight of the shore.<p> <span class="p-pull-quote__citation">-Andre Gide</span>'
+quotes[12]='<h3>Man cannot discover new oceans unless he has the courage to lose sight of the shore.</h3>'
 
-quotes[13]='<h3>The wise man does at once what the fool does finally.<p> <span class="p-pull-quote__citation">-Baltasar Gracian</span>'
+quotes[13]='<h3>The wise man does at once what the fool does finally.</h3>'
 
-quotes[14]='<h3>The world has the habit of making room for the man whose actions show that he knows where he is going.<p> <span class="p-pull-quote__citation">-Napoleon Hill</span>'
+quotes[14]='<h3>The world has the habit of making room for the man whose actions show that he knows where he is going.</h3>'
 
-quotes[15]='<h3>Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit.<p> <span class="p-pull-quote__citation">-Conrad Hilton</span>'
+quotes[15]='<h3>Success seems to be connected with action. Successful men keep moving. They make mistakes, but they don\'t quit.</h3>'
 
-quotes[16]='<h3>Do the things you know, and you shall learn the truth you need to know.<p> <span class="p-pull-quote__citation">-George Macdonald</span>'
+quotes[16]='<h3>Do the things you know, and you shall learn the truth you need to know.</h3>'
 
-quotes[17]='<h3>I have never heard anything about the resolutions of the apostles, but a good deal about their acts.<p> <span class="p-pull-quote__citation">-Horace Mann</span>'
+quotes[17]='<h3>I have never heard anything about the resolutions of the apostles, but a good deal about their acts.</h3>'
 
-quotes[18]='<h3>Let us not be content to wait and see what will happen, but give us the determination to make the right things happen.<p> <span class="p-pull-quote__citation">-Peter Marshall</span>'
+quotes[18]='<h3>Let us not be content to wait and see what will happen, but give us the determination to make the right things happen.</h3>'
 
-quotes[19]='<h3>I hear and I forget, I see and I remember. I do and I understand.<p> <span class="p-pull-quote__citation">-Chinese Proverb</span>'
+quotes[19]='<h3>I hear and I forget, I see and I remember. I do and I understand.</h3>'
 
-quotes[20]='<h3>One may walk over the highest mountain one step at a time.<p> <span class="p-pull-quote__citation">-John Wanamaker</span>'
+quotes[20]='<h3>One may walk over the highest mountain one step at a time.</h3>'
 
-quotes[21]='<h3>Every action is either strong or weak, and when every action is strong we are successful.<p> <span class="p-pull-quote__citation">-Wallace D. Wattles</span>'
+quotes[21]='<h3>Every action is either strong or weak, and when every action is strong we are successful.</h3>'
 
-quotes[22]='<h3>If we do what is necessary, all the odds are in our favor.<p> <span class="p-pull-quote__citation">-Henry Kissinger</span>'
+quotes[22]='<h3>If we do what is necessary, all the odds are in our favor.</h3>'
 
-quotes[23]='<h3>Little minds are tamed and subdued by misfortune; but great minds rise above them.<p> <span class="p-pull-quote__citation">-Washington Irving</span>'
+quotes[23]='<h3>Little minds are tamed and subdued by misfortune; but great minds rise above them.</h3>'
 
-quotes[24]='<h3>When an affliction happens to you, you either let it defeat you, or you defeat it.<p> <span class="p-pull-quote__citation">-Rosalind Russell</span>'
+quotes[24]='<h3>When an affliction happens to you, you either let it defeat you, or you defeat it.</h3>'
 
-quotes[25]='<h3>There\'s a basic human weakness inherent in all people which tempts them to want what they can\'t have and not want what is readily available to them.<p> <span class="p-pull-quote__citation">-Robert J. Ringer</span>'
+quotes[25]='<h3>There\'s a basic human weakness inherent in all people which tempts them to want what they can\'t have and not want what is readily available to them.</h3>'
 
-quotes[26]='<h3>If there is something to gain and nothing to lose by asking, by all means ask.<p> <span class="p-pull-quote__citation">-W. Clement Stone</span>'
+quotes[26]='<h3>If there is something to gain and nothing to lose by asking, by all means ask.</h3>'
 
-quotes[27]='<h3>It\'s not the situation ... It\'s your reaction to the situation.<p> <span class="p-pull-quote__citation">-Robert Conklin</span>'
+quotes[27]='<h3>It\'s not the situation ... It\'s your reaction to the situation.</h3>'
 
-quotes[28]='<h3>Life at any time can become difficult: life at any time can become easy.  It all depends upon how one adjusts oneself to life.<p> <span class="p-pull-quote__citation">-Morarji Desai</span>'
+quotes[28]='<h3>Life at any time can become difficult: life at any time can become easy.  It all depends upon how one adjusts oneself to life.</h3>'
 
-quotes[29]='<h3>What happens is not as important as how you react to what happens.<p> <span class="p-pull-quote__citation">-Thaddeus Golas</span>'
+quotes[29]='<h3>What happens is not as important as how you react to what happens.</h3>'
 
-quotes[30]='<h3>To hell with circumstances; I create opportunities.<p> <span class="p-pull-quote__citation">-Bruce Lee</span>'
+quotes[30]='<h3>To hell with circumstances; I create opportunities.</h3>'
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
 document.write(quotes[whichquote])
